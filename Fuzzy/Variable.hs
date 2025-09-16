cd7a58ef50dd80ce0a5346ecf70f33ef58228e60
silvermannn <227780093+silvermannn@users.noreply.github.com>
2025-09-16 16:04:25 +0300

    Fixed 'not'

--- Fuzzy/Variable.hs
+++ Fuzzy/Variable.hs
@@ -4,7 +4,7 @@ import Fuzzy.Function.Function
 
 class (Function v a b, Num b, Ord b) => FuzzyVariable v a b where
     not :: v a b -> v a b
-    not v = transform negate v
+    not v = transform (1-) v
 
     or :: v a b -> v a b -> v a b
     or v1 v2 = combine max v1 v2
